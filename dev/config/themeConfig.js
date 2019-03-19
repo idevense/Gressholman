7d4d6c27b1a8c90ab6a506261ba651c4942e16c0
Idar Evensen <idar@iedev.no>
2019-03-19 23:07:12 +0100

    project start

---
+++ dev/config/themeConfig.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+	theme: {
+		slug: 'Gressholman',
+		name: 'Gressholman',
+		author: 'Idar Evensen'
+	},
+	dev: {
+		browserSync: {
+			live: true,
+			proxyURL: 'http://localhost/wordpress/',
+			bypassPort: '8181'
+		},
+		browserslist: [ // See https://github.com/browserslist/browserslist
+			'> 1%',
+			'last 2 versions'
+		],
+		debug: {
+			styles: false, // Render verbose CSS for debugging.
+			scripts: false // Render verbose JS for debugging.
+		}
+	},
+	export: {
+		compress: true
+	}
+};
